febeab62dc7aee49ca29cba70229285598a357dc
isepirmansyah <isepirmansyah29@gamil.com>
2022-12-11 16:47:18 +0700

    Update task 10

---
+++ pertemuan-10/synchronous.js
@@ -0,0 +1,9 @@
+console.log("Isep Membuka Bowser Google Chrome.");
+
+console.log(`
+Isep download file selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("Isep membuka Youtube.");
